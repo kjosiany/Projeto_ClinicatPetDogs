9f9185807df1663cbcd3aa3b1e23ceafe305b105
Josisny Karla <kjosianyk@gmail.com>
2026-04-28 21:38:22 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const sqlite3 = require("sqlite3").verbose();
 
 const db = new sqlite3.Database(
-  "C:/ProjetosSql/sqlite-tools-win-x64-3530000/clinica.db",
+  "./clinica.db",
   (err) => {
     if (err) {
       console.error("Erro ao conectar:", err.message);
